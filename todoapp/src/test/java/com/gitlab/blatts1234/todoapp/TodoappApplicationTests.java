454cc58947de8f81c2a495bec1765e96601d6b38
Jakub <brofix@protonmail.com>
2021-03-24 11:03:13 +0100

    start of backend

---
+++ todoapp/src/test/java/com/gitlab/blatts1234/todoapp/TodoappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gitlab.blatts1234.todoapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
